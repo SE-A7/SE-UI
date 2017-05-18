e2a29792638606e19794a1186cd0f1f312ecd3fe
BogdanB01 <bocabogdan2010@yahoo.com>
2017-05-18 14:08:52 +0300

    Added application launcher

--- src/src/sample/Main.java
+++ src/src/sample/Main.java
@@ -2,8 +2,12 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,9 +15,31 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("XWiki Convertor");
+
+        VBox layout = new VBox();
+        layout.setAlignment(Pos.BASELINE_CENTER);
+        layout.setSpacing(350);
+
+        Text label = new Text("Welcome to XWiki Convertor");
+        label.setId("welcome");
+        Button okButton = new Button("Let's start");
+
+        okButton.setOnAction(e-> System.out.println("Switch scene"));
+        okButton.setId("ok");
+        layout.getChildren().addAll(label, okButton);
+
+
+        layout.setId("root");
+
+        Scene scene = new Scene(layout, 800, 600);
+
+        String css = this.getClass().getResource("../resources/style.css").toExternalForm();
+        scene.getStylesheets().add(css);
+
+        primaryStage.setScene(scene);
         primaryStage.show();
+        primaryStage.setResizable(false);
     }
 
 
