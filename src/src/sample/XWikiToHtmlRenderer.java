db5239a6ff20dc9bd59b227e07f37f1adf88d396
cristid9 <cristi.dnla@gmail.com>
2017-05-19 00:19:09 +0300

    Added basic implementation for the renderer

---
+++ src/src/sample/XWikiToHtmlRenderer.java
@@ -0,0 +1,36 @@
+package sample;
+
+
+
+import org.xwiki.component.embed.EmbeddableComponentManager;
+import org.xwiki.component.manager.ComponentLookupException;
+import org.xwiki.rendering.converter.ConversionException;
+import org.xwiki.rendering.converter.Converter;
+import org.xwiki.rendering.renderer.printer.DefaultWikiPrinter;
+import org.xwiki.rendering.renderer.printer.WikiPrinter;
+import org.xwiki.rendering.syntax.Syntax;
+
+import java.io.StringReader;
+
+public class XWikiToHtmlRenderer {
+    public static String convert(String xWikiString) {
+        EmbeddableComponentManager componentManager = new EmbeddableComponentManager();
+        componentManager.initialize(XWikiToHtmlRenderer.class.getClassLoader());
+
+        Converter converter = null;
+        try {
+            converter = componentManager.getInstance(Converter.class);
+        } catch (ComponentLookupException e) {
+            e.printStackTrace();
+        }
+
+        WikiPrinter printer = new DefaultWikiPrinter();
+        try {
+            converter.convert(new StringReader(xWikiString), Syntax.XWIKI_2_1, Syntax.XHTML_1_0, printer);
+        } catch (ConversionException e) {
+            e.printStackTrace();
+        }
+
+        return printer.toString();
+    }
+}
