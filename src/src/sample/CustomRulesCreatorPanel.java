d19afa1b7e47e078e9699bb62f8f397d927970e5
Tudor-George Motrescu <tudor.g.motrescu@gmail.com>
2017-05-21 02:17:58 +0300

    Custom Rules change

--- src/src/sample/CustomRulesCreatorPanel.java
+++ src/src/sample/CustomRulesCreatorPanel.java
@@ -21,11 +21,21 @@ public class CustomRulesCreatorPanel {
         text.setText("\nInsert your new xWiki rule:");
         text.setFont(Font.font("verdana", FontPosture.REGULAR, 15));
         text.setFill(Color.BLACK);
-        text.setStrokeWidth(2);;
+        text.setStrokeWidth(2);
+
+        Text text2 = new Text();
+        text2.setText("\nInsert rule's purpose:");
+        text2.setFont(Font.font("verdana", FontPosture.REGULAR, 15));
+        text2.setFill(Color.BLACK);
+        text2.setStrokeWidth(2);
 
         TextArea ruleInput = new TextArea();
         ruleInput.setPrefColumnCount(30);
         ruleInput.setPrefRowCount(6);
+
+        TextArea roleInput = new TextArea();
+        roleInput.setPrefColumnCount(30);
+        roleInput.setPrefRowCount(2);
 
         Button addbtn = new Button();
         addbtn.setOnAction(event -> ErrorWindow.displayError("This is a test error!"));
@@ -43,6 +53,11 @@ public class CustomRulesCreatorPanel {
         txthb.getChildren().add(text);
         txthb.setAlignment(Pos.CENTER);
 
+        HBox txthb2 = new HBox();
+        txthb2.setSpacing(25);
+        txthb2.getChildren().add(text2);
+        txthb2.setAlignment(Pos.CENTER);
+
         HBox hb = new HBox();
         hb.setSpacing(50);
         hb.getChildren().add(ruleInput);
@@ -50,13 +65,18 @@ public class CustomRulesCreatorPanel {
 
         HBox hb2 = new HBox();
         hb2.setSpacing(50);
-        hb2.getChildren().addAll(addbtn,cancelbtn);
+        hb2.getChildren().add(roleInput);
         hb2.setAlignment(Pos.CENTER);
 
-        layout.setSpacing(30);
-        layout.getChildren().addAll(txthb,hb,hb2);
+        HBox hb3 = new HBox();
+        hb3.setSpacing(50);
+        hb3.getChildren().addAll(addbtn,cancelbtn);
+        hb3.setAlignment(Pos.CENTER);
 
-        Scene scene = new Scene(layout, 450, 200);
+        layout.setSpacing(30);
+        layout.getChildren().addAll(txthb,hb,txthb2,hb2,hb3);
+
+        Scene scene = new Scene(layout, 600, 400);
         String css = CustomRulesCreatorPanel.class.getResource("../resources/style.css").toExternalForm();
         scene.getStylesheets().add(css);
 
